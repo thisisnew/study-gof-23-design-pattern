9497147078421caa0937c4248ef0ffc5d8051c9f
thisisnew <thisisnew@naver.com>
2025-08-15 21:23:08 +0900

    파트2: 어댑터(상속대신 구현을 이용한)

--- design-patten/src/part2/adapter/Print.java
+++ design-patten/src/part2/adapter/Print.java
@@ -1,6 +1,6 @@
 package part2.adapter;
 
-public interface Print {
-    void printWeak();
-    void printStrong();
+public abstract class Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
 }
--- design-patten/src/part2/adapter/PrintBanner.java
+++ design-patten/src/part2/adapter/PrintBanner.java
@@ -1,18 +1,20 @@
 package part2.adapter;
 
-public class PrintBanner extends Banner implements Print {
+public class PrintBanner extends Print {
+
+    private final Banner banner;
 
     public PrintBanner(String string) {
-        super(string);
+        this.banner = new Banner(string);
     }
 
     @Override
     public void printWeak() {
-        showWithParen();
+        banner.showWithParen();
     }
 
     @Override
     public void printStrong() {
-        showWithAster();
+        banner.showWithAster();
     }
 }
