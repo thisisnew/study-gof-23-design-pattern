cf0cc1d9bbe4675906e2a2c824fa99590502d35a
thisisnew <thisisnew@naver.com>
2025-08-15 20:54:36 +0900

    파트2: 어댑터

---
+++ design-patten/src/part2/adapter/Main.java
@@ -0,0 +1,9 @@
+package part2.adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        Print print = new PrintBanner("Hello");
+        print.printWeak();
+        print.printStrong();
+    }
+}
---
+++ design-patten/src/part2/adapter/Print.java
@@ -0,0 +1,6 @@
+package part2.adapter;
+
+public interface Print {
+    void printWeak();
+    void printStrong();
+}
