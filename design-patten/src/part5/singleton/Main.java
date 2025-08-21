60d004f10a3c5d594aa1a0ee95d32869b9ebc965
thisisnew <thisisnew@naver.com>
2025-08-21 23:35:38 +0900

    파트5: singleton 패턴

--- design-patten/src/part5/singleton/Main.java
+++ design-patten/src/part5/singleton/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
 
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
+        var obj1 = Singleton.getInstance();
+        var obj2 = Singleton.getInstance();
 
         if (obj1 == obj2){
             System.out.println("같은 인스턴스 입니다.");
